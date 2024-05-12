305a90f737e0a7ee633948b5d95b0b2b50de7020
mithileshpradhan1 <pradhan.mithilesh123@gmail.com>
2024-05-12 12:53:17 +0530

    Added postgresql service.

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Trest the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """Test subract numbers."""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res,5)
